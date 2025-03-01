031f672116cfc8fdcd62e0fad2293367a47acb50
nischal <nischal@progatetechnology.com>
2025-03-01 11:17:48 +0530

    sample BE deplpiyment

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-const PORT = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.get("/", (req, res) => {
-    res.send("SERVER");
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
