d702e71d7a58c749742927e40ad86a425e0ec9ad
nischal <nischal@progatetechnology.com>
2025-03-01 13:15:09 +0530

    url fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = process.env.WEBSITES_PORT || 8000
+// const port = process.env.WEBSITES_PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(8000, () => {
+  console.log(`Example app listening at http://localhost:8000`)
 })
