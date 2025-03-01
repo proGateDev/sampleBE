3ecc87c97ff81159440d752c2fbd11e8fb383f2b
nischal <nischal@progatetechnology.com>
2025-03-01 13:07:45 +0530

    url fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = process.env.WEBSITES_PORT || 8080
+const port = process.env.WEBSITES_PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
