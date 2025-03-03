6c883ecb43a0b28ca9f7b43e81f780484b208a0e
nischal <nischal@progatetechnology.com>
2025-03-03 15:23:27 +0530

    code oush

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8000
+// const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
