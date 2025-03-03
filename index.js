0075feb888f48d81ccc777830d7d8da4f20dd7fe
nischal <nischal@progatetechnology.com>
2025-03-03 15:29:19 +0530

    code oush

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT | 8080, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
